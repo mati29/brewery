96c0d1e2a0e200c29462258f9378dd88cd17c6ca
Mati <mering20@gmail.com>
2020-08-31 17:23:48 +0200

    Add validation for dto

--- src/main/java/guru/springframework/brewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/brewery/web/controller/BeerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @Deprecated
@@ -24,7 +25,7 @@ public class BeerController {
     }
 
     @PostMapping//POST - create new beer
-    public ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
+    public ResponseEntity handlePost(@Valid @RequestBody BeerDto beerDto) {
         BeerDto saveDto = beerService.saveNewBeer(beerDto);
 
         HttpHeaders headers = new HttpHeaders();
@@ -35,7 +36,7 @@ public class BeerController {
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @Valid  @RequestBody BeerDto beerDto) {
         beerService.updateBeer(beerId, beerDto);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
--- src/main/java/guru/springframework/brewery/web/model/BeerDto.java
+++ src/main/java/guru/springframework/brewery/web/model/BeerDto.java
@@ -2,6 +2,9 @@ package guru.springframework.brewery.web.model;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.util.UUID;
 
 @Data
@@ -10,12 +13,16 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
 
+    @NotBlank
     private String beerName;
 
+    @NotBlank
     private String beerStyle;
 
+    @Positive
     private Long upc;
 
 }
